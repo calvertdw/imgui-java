55db05ae03b68f7d1b8fee27d749f486bd5cf009
AAstroPhysiCS <okangucluwin20@hotmail.com>
2021-05-10 16:43:02 +0200

    delete unnecessary intellij build files

--- imgui-binding/tmp/imgui/jni/_binding.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "jni_common.h"
-#include "jni_callbacks.h"
-#include "jni_binding_struct.h"
--- imgui-binding/tmp/imgui/jni/_imnodes.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "_base.h"
-#include <imnodes.h>
-#include "_binding.h"
